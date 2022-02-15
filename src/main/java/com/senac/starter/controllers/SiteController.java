524679f037360735643e3734177092069aa3be44
sala19a <sala19a@DRLAB19A42603>
2022-02-14 21:31:05 -0300

    new version

--- src/main/java/com/senac/starter/controllers/SiteController.java
+++ src/main/java/com/senac/starter/controllers/SiteController.java
@@ -1,37 +1,78 @@
 package com.senac.starter.controllers;
 
+import com.senac.starter.models.Usuario;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/")
 public class SiteController {
 
     @GetMapping("/")
-    public String home(){
+    public ModelAndView home(){
+        //System.out.println("Entrou no home");
+        ModelAndView modelAndView = new ModelAndView("home");
+        Usuario usuario =new Usuario("Marcio",20,true);
 
-        return "home";
+
+        String casado;
+        if (usuario.isCasado()) {
+            casado = " Verificamos que você  casado.";
+        } else {
+            casado = " Verificamos que você não é casado.";
+
+        }
+        String saudacao = "Bem vindo ";
+        String texto = " agora são ";
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        var str_hora = localDateTime.getHour() + " Horas e " + localDateTime.getMinute() + " Minutos.";
+        String frase = saudacao + usuario.getNome() + texto + str_hora + casado;
+
+
+        modelAndView.addObject("frase",frase);
+
+
+        ;
+
+
+
+
+
+
+
+
+      // modelAndView.addObject("texto",texto);
+        return modelAndView;
     }
 
     @GetMapping("/jogos")
     public String jogos(){
+        System.out.println("Entrou no jogos");
         return "jogos";
     }
 
     @GetMapping("/filmes")
     public String filmes(){
-
+        System.out.println("Entrou no filmes");
         return "filmes";
     }
 
     @GetMapping("/posts")
     public String posts(){
+        System.out.println("Entrou no posts");
         return "posts";
     }
 
     @GetMapping("/contato")
     public String contato(){
+        System.out.println("Entrou no contato");
         return "contato";
     }
 }
